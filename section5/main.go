1dabb8b175a44c42d950971a7ff4e7e087bf509b
daikiku10 <soccerdk19.910@gmail.com>
2022-04-27 11:15:26 +0900

    定数について

---
+++ section5/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+/** 定数 */
+
+const Pi = 3.14
+
+const (
+	URL = "https://..."
+	SiteName = "Google"
+)
+
+func main() {
+
+	fmt.Println(Pi)
+
+	fmt.Println(URL, SiteName)
+
+}
